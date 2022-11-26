f6021d7a7c724ad1eb21cdb9c5b7e90185820f46
Helen <lenapukhova3m96@mail.ru>
2022-11-26 21:50:44 +0300

    add Tasks

---
+++ Task1/Program.cs
@@ -0,0 +1,26 @@
+﻿/*Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее,
+а какое меньшее.
+
+a = 5; b = 7 -> max = 7
+a = 2 b = 10 -> max = 10
+a = -9 b = -3 -> max = -3
+*/
+
+Console.WriteLine("Введите первое число: ");
+string number1Str = Console.ReadLine();
+int number1 = Convert.ToInt32(number1Str);
+
+Console.WriteLine("Введите второе число: ");
+string number2Str = Console.ReadLine();
+int number2 = Convert.ToInt32(number2Str);
+
+
+if(number1 > number2) 
+{
+    Console.WriteLine($"Максимальное чилсо {number1}, Минимальное число {number2}");
+}
+else if(number2 > number1)
+{
+    Console.WriteLine($"Максимальное чилсо {number2}, Минимальное число {number1}");
+}
+
---
+++ Task2/Program.cs
@@ -0,0 +1,41 @@
+﻿/*Напишите программу, которая принимает на вход три числа 
+и выдаёт максимальное из этих чисел.
+
+2, 3, 7 -> 7
+44 5 78 -> 78
+22 3 9 -> 22
+*/
+
+Console.WriteLine("Введите первое число: ");
+string number1Str = Console.ReadLine();
+int number1 = Convert.ToInt32(number1Str);
+
+Console.WriteLine("Введите второе число: ");
+string number2Str = Console.ReadLine();
+int number2 = Convert.ToInt32(number2Str);
+
+Console.WriteLine("Введите третье число: ");
+string number3Str = Console.ReadLine();
+int number3 = Convert.ToInt32(number3Str);
+
+if(number1 > number2)
+{
+    if(number1 > number3)
+    {
+        Console.WriteLine($"Максимальное чилсо {number1}");
+    }
+
+    else 
+    {
+    Console.WriteLine($"Максимальное чилсо {number3}");
+    }
+}
+
+else if(number2 > number3)
+{
+    Console.WriteLine($"Максимальное чилсо {number2}");
+}
+else
+{
+    Console.WriteLine($"Максимальное чилсо {number3}");
+}
